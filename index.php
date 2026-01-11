a1a8d8bc82cdff168913333158df30ca53dc02a4
dnskts <dnskts@mail.ru>
2026-01-11 13:33:14 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Главная страница</title>
+</head>
+<body>
+    <h1>Список страниц</h1>
+    <ul>
+        <li><a href="page1.php">Страница 1 — Ошибка 404</a></li>
+        <li><a href="page2.php?text=Привет, это текст из файла!">Страница 2 — Скачать файл</a></li>
+        <li><a href="page3.php">Страница 3 — Счётчик посещений</a></li>
+        <li><a href="page4.php">Страница 4 — Результат счётчика</a></li>
+    </ul>
+</body>
+</html>
